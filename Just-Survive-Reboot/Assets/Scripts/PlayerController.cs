7ebd0566208fa423381c66f4f3e2cc2e6fa2e43b
Z4CH4RYSNP <zs20np@snpsteam.com>
2024-01-23 17:54:57 -0500

    movement update 1

--- Just-Survive-Reboot/Assets/Scripts/PlayerController.cs
+++ Just-Survive-Reboot/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController cc;
     private Vector3 move = Vector3.zero;
-    private Vector2 input = Vector2.zero;
+    private Vector3 rawInput = Vector3.zero;
+    private Vector3 processedInput = Vector3.zero;
 
     private float mouseSens = 1f;
 
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float gravity = 5f;
     [SerializeField] private float yVel = 0;
+    [SerializeField] private float airMovementAccel = 0.5f;
 
     private Camera cam;
 
@@ -34,18 +36,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));                        //get the axis from WASD
-        if(input.magnitude > 1f) {                                                                          //I do this if statement because if I don't it messes with deceleration of the cc
-            input.Normalize(); 
+        rawInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));                        //get the axis from WASD
+
+        processedInput = transform.forward * (Mathf.Sign(rawInput.z) * 1) + transform.right * (Mathf.Sign(rawInput.x) * 1);     //I don't like the gliding effect of Unity's input system. This forces the inputs to be 1 or -1.
+        if (rawInput.z == 0) {
+            processedInput = transform.forward * 0 + transform.right * (Mathf.Sign(rawInput.x) * 1);                         //deal with edge cases of zeros
         }
-        move = transform.forward * input.y + transform.right * input.x;                                     //multiply by movement speed
-        move = move * movementSpeed;
+        if (rawInput.x == 0) {
+            processedInput = transform.forward * (Mathf.Sign(rawInput.z) * 1) + transform.right * 0;
+        }
+        if (rawInput.z == 0 && rawInput.x == 0) {
+            processedInput = transform.forward * 0 + transform.right * 0;
+        }
+        if(cc.isGrounded) {
+            processedInput.Normalize();
+            move = processedInput * movementSpeed;    //multiply by movement speed
+        } else {
+            move = Vector3.Lerp(move, processedInput * (movementSpeed), airMovementAccel * Time.deltaTime);
+        }
+
 
 
         rotationX += -Input.GetAxis("Mouse Y") * mouseSens;                                             //rotation for the camera based on Mouse Y, * sensitivity
         rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);                                    //avoid flipping >180 degrees
         cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);                                //do camera rotation
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * mouseSens, 0);             //rotate player based on Mouse X
+
+
 
         if (!cc.isGrounded) {
             yVel -= gravity * Time.deltaTime;
@@ -58,6 +75,8 @@ public class PlayerController : MonoBehaviour
         }
 
         move.y = yVel;
+
+
 
         cc.Move(move * Time.deltaTime * movementSpeed);                                                     //execute move
     }
